332a541d516626431d201c56f621e305da972a9c
hakkimS <84006367+hakkimS@users.noreply.github.com>
2021-06-24 13:00:33 +0530

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,3 +1,15 @@
 def call() {
-sh "echo world"
-}
+ logParser parsingRulesPath: '/var/lib/jenkins/log_rules/config', projectRulePath: '', useProjectRule: false
+                if (currentBuild.currentResult == 'FAILURE') {
+                    // Send an email only if the build has failed
+                    emailext subject: '$DEFAULT_SUBJECT' ,
+                        body: '$DEFAULT_CONTENT',
+                        recipientProviders: [
+                            [$class: 'CulpritsRecipientProvider'],
+                            [$class: 'DevelopersRecipientProvider'],
+                            [$class: 'RequesterRecipientProvider']
+                        ],
+                        replyTo: '$DEFAULT_REPLYTO',
+                        to: '$DEFAULT_RECIPIENTS'
+                }
+            }
