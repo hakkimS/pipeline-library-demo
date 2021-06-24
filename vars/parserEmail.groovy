67be7ee9b748ff98ecceefbd572ae1df73f2398d
hakkimS <84006367+hakkimS@users.noreply.github.com>
2021-06-24 12:35:10 +0530

    Update parserEmail.groovy

--- vars/parserEmail.groovy
+++ vars/parserEmail.groovy
@@ -1,13 +1,4 @@
 def email_error() {
  logParser parsingRulesPath: '/var/lib/jenkins/log_rules/config', projectRulePath: '', useProjectRule: false
                     // Send an email only if the build has failed
-                    emailext subject: '$DEFAULT_SUBJECT' ,
-                        body: '$DEFAULT_CONTENT',
-                        recipientProviders: [
-                            [$class: 'CulpritsRecipientProvider'],
-                            [$class: 'DevelopersRecipientProvider'],
-                            [$class: 'RequesterRecipientProvider']
-                        ], 
-                        replyTo: '$DEFAULT_REPLYTO',
-                        to: '$DEFAULT_RECIPIENTS'
-            }
+}
