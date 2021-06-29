9dec278fce87af7d8b91cc35a6737c2bfe80e231
hakkimS <84006367+hakkimS@users.noreply.github.com>
2021-06-29 09:27:24 +0530

    Update Emails.groovy

--- src/com/cleverbuilder/Emails.groovy
+++ src/com/cleverbuilder/Emails.groovy
@@ -3,6 +3,16 @@ package com.cleverbuilder
 
 class Emails {
   static def consoleParser() {
-                    println("console parser")
+                    logParser parsingRulesPath: '/var/lib/jenkins/log_rules/config', projectRulePath: '', useProjectRule: false
+                    // Send an email only if the build has failed
+                    emailext subject: '$DEFAULT_SUBJECT' ,
+                        body: '$DEFAULT_CONTENT',
+                        recipientProviders: [
+                            [$class: 'CulpritsRecipientProvider'],
+                            [$class: 'DevelopersRecipientProvider'],
+                            [$class: 'RequesterRecipientProvider']
+                        ],
+                        replyTo: '$DEFAULT_REPLYTO',
+                        to: '$DEFAULT_RECIPIENTS'
             }
 }
