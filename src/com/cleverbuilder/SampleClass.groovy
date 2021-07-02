f747fbe599714eacb79355d0512ce886501201eb
hakkimS <84006367+hakkimS@users.noreply.github.com>
2021-07-02 07:47:28 +0530

    Update SampleClass.groovy

--- src/com/cleverbuilder/SampleClass.groovy
+++ src/com/cleverbuilder/SampleClass.groovy
@@ -6,7 +6,7 @@ class SampleClass {
      def temp = libraryResource 'org/sample.sh'
      writeFile file:"test.sh",text:temp
      sh 'chmod +x test.sh'
-     def int count = sh './test.sh'
+     def int count = 1
   if (!Primes.isPrime(count)) {
     error "${count} was not prime"
   } else {
