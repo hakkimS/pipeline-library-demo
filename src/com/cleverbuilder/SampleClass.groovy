1c39a59559367032be154137c617bc831f92b2e6
hakkimS <84006367+hakkimS@users.noreply.github.com>
2021-07-02 08:05:33 +0530

    Update SampleClass.groovy

--- src/com/cleverbuilder/SampleClass.groovy
+++ src/com/cleverbuilder/SampleClass.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/env groovy
 package com.cleverbuilder
 class SampleClass {
-   def numberSequence(){
+   def numberSequence() {
      def temp = libraryResource 'org/sample.sh'
      writeFile file:"test.sh",text:temp
      sh 'chmod +x test.sh'
