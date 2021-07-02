952449456ca430c44cecb9d41d658baf760cb3b4
hakkimS <84006367+hakkimS@users.noreply.github.com>
2021-07-02 08:00:49 +0530

    Update SampleClass.groovy

--- src/com/cleverbuilder/SampleClass.groovy
+++ src/com/cleverbuilder/SampleClass.groovy
@@ -1,8 +1,5 @@
 #!/usr/bin/env groovy
 package com.cleverbuilder
-@Grab('org.apache.commons:commons-math3:3.4.1')
-import org.apache.commons.math3.primes.Primes
-
 class SampleClass {
    def numberSequence(){
      def temp = libraryResource 'org/sample.sh'
