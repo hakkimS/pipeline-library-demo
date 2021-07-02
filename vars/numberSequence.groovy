d7332baee8961dcc7ccb20b5892859936bc1b671
hakkimS <84006367+hakkimS@users.noreply.github.com>
2021-07-02 07:34:01 +0530

    Update numberSequence.groovy

--- vars/numberSequence.groovy
+++ vars/numberSequence.groovy
@@ -2,4 +2,10 @@ def call(){
      def temp = libraryResource 'org/sample.sh'
      writeFile file:"test.sh",text:temp
      sh 'chmod +x test.sh'
+     def int count = sh './test.sh'
+  if (!Primes.isPrime(count)) {
+    error "${count} was not prime"
+  } else {
+    echo "${count} is a prime"
+  }
 }
