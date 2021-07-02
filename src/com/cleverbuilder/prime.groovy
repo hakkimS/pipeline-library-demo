8eef944173f6b594139bb30e0d53c54285bf35eb
hakkimS <84006367+hakkimS@users.noreply.github.com>
2021-07-02 07:59:09 +0530

    Update prime.groovy

--- src/com/cleverbuilder/prime.groovy
+++ src/com/cleverbuilder/prime.groovy
@@ -1,2 +1,8 @@
 @Grab('org.apache.commons:commons-math3:3.4.1')
 import org.apache.commons.math3.primes.Primes
+  def int count = 1
+  if (!Primes.isPrime(count)) {
+    error "${count} was not prime"
+  } else {
+    echo "${count} is a prime"
+  }
