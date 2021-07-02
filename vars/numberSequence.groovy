e98526e1e36f5567a82d29b327c858e3203ea30d
hakkimS <84006367+hakkimS@users.noreply.github.com>
2021-07-02 07:03:01 +0530

    Update numberSequence.groovy

--- vars/numberSequence.groovy
+++ vars/numberSequence.groovy
@@ -2,5 +2,5 @@ def call(){
      def temp = libraryResource 'org/sample.sh'
      writeFile file:"test.sh",text:temp
      sh 'chmod +x test.sh
-     def seqno = sh "./test.sh"
+     def seqno = sh './test.sh'
 }
